e1bf90f7a18719fd56fa70631eeccb7c65807fa2
psi <ledyba@users.sourceforge.jp>
2013-07-26 21:59:46 +0900

    Signed-off-by: psi <ledyba@users.sourceforge.jp>

---
+++ src/test/scala/sample/SampleSuite.scala
@@ -0,0 +1,18 @@
+package sample
+
+import org.scalatest.junit.AssertionsForJUnit
+import scala.collection.mutable.ListBuffer
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+class SampleSuite extends AssertionsForJUnit {
+	@Before def initialize() {
+	}
+
+	@Test def verifyEasy() { // Uses JUnit-style assertions
+	}
+
+	@Test def verifyFun() { // Uses ScalaTest assertions
+	}
+}
